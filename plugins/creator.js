7eeb16ca4cbee3c462d8bcce4dd7620c4c7e852d
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-12-24 14:45:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,20 +5,20 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;AbiDev11\nNICKNAME:Owner\nORG:AbiDev11\nTITLE:soft\nitem1.TEL;waid=6288232154376:+62 882-3215-4376\nitem1.X-ABLabel:📞 Nomor\nitem2.URL:https://github.com/AbiDev11\nitem2.X-ABLabel:More\nitem3.EMAIL;type=INTERNET: abiputrapratama34@gmail.com\nitem3.X-ABLabel:Gmail\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABADR:More\nitem4.X-ABLabel:Lokasi\nBDAY;value=date:13 January 2001\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;ega\nNICKNAME:Owner\nORG:AbiDev11\nTITLE:soft\nitem1.TEL;waid=6283849469400:+62-838-4946-9400\nitem1.X-ABLabel:📞 Nomor\nitem2.URL:https://github.com/Doramisi1\nitem2.X-ABLabel:More\nitem3.EMAIL;type=INTERNET: egabertoananta65@gmail.com\nitem3.X-ABLabel:Gmail\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABADR:More\nitem4.X-ABLabel:Lokasi\nBDAY;value=date:13 January 2001\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner Saya kak`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: fkontak, mentions: conn.parseMention(caption) })
 }
 if (command == 'pengembang') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6288232154376:+62 882-3215-4376\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6283802603905:+62-838-0260-3905\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
 }
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `Developer Bot `, `Don't call me`, `abiputrapratama34`, `Indonesia`, `https://github.com/AbiDev11`, `Bapaknya Loh lucu banget rek😁`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `Bot WhatsApp`, `Don't spam/call me`, `Nothing`, `Indonesia`, `https://github.com/AbiDev11`, `Maklor sebaskom full😱`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `Developer Bot `, `Don't call me`, `egabertoananta65`, `Indonesia`, `https://github.com/Doramisi1`, `Bapaknya Loh lucu banget rek😁`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `Bot WhatsApp`, `Don't spam/call me`, `Nothing`, `Indonesia`, `https://github.com/Doramisi1`, `Maklor sebaskom full😱`]
   ], fkontak)
   await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
   } catch {
