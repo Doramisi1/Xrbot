7ca91176eeb71c08faee036741bf25f4061598e2
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-12-24 16:35:57 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,16 +1,18 @@
-import xa from 'xfarr-api'
-
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-
-   if (!args[0]) throw `Imput Url TikTok`
-   let res = await xa.downloader.tiktok(args[0])
-  m.reply(wait)
-  await conn.sendFile(m.chat, res.media[0].url, '', `${sym} *Title :* ${res.title}\n\n © 𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝑩𝒚 ${res.author}`, m)
+const { tiktokdl } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+*Description:* ${description}
+`.trim(), m)
 }
-handler.help = ['tiktok <url>']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^(tiktok|tt)$/i
-handler.limit = true
 
-export default handler
+handler.limit = true
+module.exports = handler
